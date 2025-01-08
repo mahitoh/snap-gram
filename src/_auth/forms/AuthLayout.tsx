f85ce78cdbd34b355fef21e73f80c7e9a5e993c6
mahitoh <richcalchia@gmail.com>
2025-01-08 19:04:36 +0100

    feat: add new UI components and Appwrite integrati
    - Added new UI components: form, input, label, and shared Loader
    - Integrated Appwrite API and configuration
    - Added validation utilities
    - Updated existing authentication forms and global styles

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -8,13 +8,13 @@ function AuthLayout() {
         <Navigate to="/" />
       ) : (
         <>
-          <section className="flex flex-1 justify-center  items-center flex-col py-10">
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
           <img
           src="./assets/images/side-img.svg"
           alt="logo"
-          className=" xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          className="hidden lg:block xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
         </>
       )}
